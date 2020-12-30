c33e48ed0166d2c3db2b4286af84b1e28bcd7a6f
Dmitrii Kalyuzhnyi <dskeygard@mail.ru>
2020-12-30 22:29:29 +0300

    Init Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Animator animator;
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+    
+    
+
+    private Rigidbody2D rb;
+
+    private bool facingRight = true;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float chekRadius;
+    public LayerMask whatIsGround; 
+
+
+    private int extraJumps;
+    public int extraJumpsValue;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        extraJumps = extraJumpsValue;
+        rb = GetComponent<Rigidbody2D>(); 
+    }
+
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, chekRadius, whatIsGround);                  
+
+
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        
+        if (animator)
+        {
+            animator.SetBool("Walk", Mathf.Abs(moveInput) >= 0.1f);
+        }
+
+        if (facingRight == false && moveInput > 0)
+        {
+            Flip();
+        } 
+        else if (facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+    }
+
+    private void Update()
+    {
+        if (isGrounded == true)
+        {
+            extraJumps = extraJumpsValue;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && extraJumps > 0)
+        {   
+            if (animator) {
+                animator.SetTrigger("Jump");
+            }
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }else if (Input.GetKeyDown(KeyCode.Space) && extraJumps == 0 && isGrounded ==true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 180f, 0f);
+    }
+}
