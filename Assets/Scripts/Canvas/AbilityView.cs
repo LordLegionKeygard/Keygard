3435c7967a756d990f3632cd5af510029b8738f3
UninixStudio <UninixStudio@gmail.com>
2021-03-28 15:34:47 +0200

    Refactoring Абилок в канвасе

---
+++ Assets/Scripts/Canvas/AbilityView.cs
@@ -0,0 +1,23 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class AbilityView
+{
+	public Image Fill;
+	public KeyCode Key;
+	public float Cooldown;
+
+	public void Update()
+	{
+		if (Input.GetKeyDown(Key))
+			FillImage();
+	}
+
+	private void FillImage()
+	{
+		Fill.fillAmount = 1f;
+		Fill.DOFillAmount(0f, Cooldown);
+	}
+}
